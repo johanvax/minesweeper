086b257ef116fce168ac671b77a621dadb744040
johanvax <152924301+johanvax@users.noreply.github.com>
2025-06-04 19:58:18 +0200

    tried to make a binary

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     rows = int(args[1]) if len(args) > 1 else 30
     cols = int(args[2]) if len(args) > 1 else 40
 
-    path = __file__[:-7] # remove "main.py"
+    path = __file__[:-7] # remove "main.py", if you run the code yourself
+    # path = './'
     game = Game(rows, cols, path)
     game.run()
